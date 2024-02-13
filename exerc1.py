750b03cdcae5406c919e7167bb4917ea37885159
Kaian Samuel <kaiansamuelster@gmail.com>
2024-02-13 08:53:56 -0300

    Aula 1 e exercicio1

---
+++ exerc1.py
@@ -0,0 +1,17 @@
+import os
+
+print('===== Bem vindo ao sistema de cadastro =====')
+print('Digite o seu nome: ')
+nome = input()
+print('Digite seu telefone: ')
+telefone = input()
+print('Digite seu endereco: ')
+endereco = input()
+
+os.system('clear')
+
+print('====Programa de formatação de dados ====')
+print('Nome ' + nome)
+print('Telefone ' + telefone)
+print('Endereço ' + endereco)
+print('=================')
